2a4802954d9245725c399787af0ffbde00bece8f
tamerbulutt <tamerbuluttt@gmail.com>
2021-03-03 23:44:24 +0300

    ASP.NET MVC Entity Framework Code First Approach
    We create database with ASP.NET MVC Entity Framework Code First approach

---
+++ CodeFirst/CodeFirst/Models/Context/CodeFirstContext.cs
@@ -0,0 +1,15 @@
+﻿using CodeFirst.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Models.Context
+{
+    public class CodeFirstContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ CodeFirst/CodeFirst/Models/Entity/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Models.Entity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
